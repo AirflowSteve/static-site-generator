c05c97147cb1ada19b9d39fc6e6a74eddeeac650
AirflowSteve <nlifanov02@gmail.com>
2026-05-18 23:13:50 +0300

    added functionality to generate multiple web-pages

---
+++ src/generate_pages.py
@@ -0,0 +1,35 @@
+from markdown_blocks import *
+from extract_header import extract_title
+import os
+
+def generating_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as f:
+        markdown = f.read()
+    with open(template_path) as f:
+        template = f.read()
+    md_node = markdown_to_html_node(markdown)
+    html = md_node.to_html()
+    title = extract_title(markdown)
+    html_page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    dirs_to_create = "".join(dest_path.split("/")[:-1])
+    if not os.path.exists(dirs_to_create):
+        os.makedirs(dirs_to_create)
+    file = open(dest_path, "w")
+    file.write(html_page)
+    file.close()
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    directories = os.listdir(dir_path_content)
+    for item in directories:
+        item_path = os.path.join(dir_path_content, item)
+        dest_path = os.path.join(dest_dir_path, item)
+        print(directories, item_path, dest_path)
+        if os.path.isfile(item_path):
+            dest_path = dest_path.replace(".md", ".html")
+            generating_page(item_path, template_path, dest_path)
+            continue
+        os.mkdir(dest_path)
+        
+        generate_pages_recursive(item_path, template_path, dest_path)
+    return None
